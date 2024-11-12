9efdffba4e6a28780395677442d27a07def724fa
Guilherme Figueiredo <guilherme.figueiredo@school.sptech>
2024-11-12 13:37:50 -0300

    correção do commit anterior

---
+++ versão com API/src/database/script-tabelas.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE FinGui;
+USE FinGui;
+
+CREATE TABLE cliente_cadastro (
+    idCliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+	cpf CHAR(11),
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(255)
+);
+
+
+CREATE TABLE itens_financas (
+    idItem INT PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(45),
+    valor DECIMAL(10,2),
+    tipo VARCHAR(8),
+    fkCliente INT,
+    CONSTRAINT chkTipo CHECK (tipo IN ('entrada', 'saida')),
+    FOREIGN KEY (fkCliente) REFERENCES cliente_cadastro(idCliente)
+);
+select * from cliente_cadastro;
+-- SELECT 
+--     c.nome AS cliente,
+--     c.email,
+--     i.descricao,
+--     i.valor,
+--     i.tipo
+-- 	FROM itens_financas i
+-- 	JOIN cliente_cadastro c 
+-- 	ON i.fkCliente = c.idCliente 
+-- 	ORDER BY c.nome;
---
+++ versão com API/src/models/usuarioModel.js
@@ -0,0 +1,38 @@
+var database = require("../database/config");
+
+function autenticar(email, senha) {
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ",
+    email,
+    senha
+  );
+  var instrucaoSql = `
+        SELECT idCliente, nome, cpf, email, senha FROM cliente_cadastro WHERE email = '${email}' AND senha = '${senha}';
+    `;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, cpf, email, senha) {
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
+    nome,
+    cpf,
+    email,
+    senha,
+  );
+
+  // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+  //  e na ordem de inserção dos dados.
+  var instrucaoSql = `
+        INSERT INTO cliente_cadastro (nome, cpf, email, senha) VALUES ('${nome}', '${cpf}', '${email}', '${senha}');
+    `;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+module.exports = {
+  autenticar,
+  cadastrar,
+};
